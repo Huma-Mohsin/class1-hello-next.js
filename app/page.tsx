6fc89062b9599a48a6970bcdb0b85b298fe44d16
Huma-Mohsin <160951214+Huma-Mohsin@users.noreply.github.com>
2024-10-02 20:34:49 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+
+      <div style={{ textAlign: 'center', margin: '20px' }}>
+  <h1 style={{ fontSize: '2.5rem', color: '#0070f3' }}>Hello Next.js</h1>
+  <p style={{ fontSize: '1.2rem', color: '#333' }}>
+    This is my first experience with Next.js. I'm excited to explore its features and capabilities!
+  </p>
+</div>
+
+   
+  );
+}
